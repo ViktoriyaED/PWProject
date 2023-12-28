74d49afb757cfd9f9f640be8cc8f26a7c7ca617b
ViktoriyaED <qaviktoriaed@gmail.com>
2023-12-28 14:06:59 -0500

    Build properties if ci-runs

--- src/test/java/base/BaseTest.java
+++ src/test/java/base/BaseTest.java
@@ -7,6 +7,7 @@ import org.testng.annotations.*;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.lang.reflect.Method;
 import java.nio.file.Paths;
 import java.util.Properties;
@@ -17,25 +18,31 @@ public class BaseTest {
     private Browser browser;
     private BrowserContext context;
     protected Page page;
-    protected Properties properties;
+    protected static Properties properties;
+
+    private static final String ENV_WEB_OPTIONS = "WEB_OPTIONS";
+
+    private static final String ENV_BROWSER_OPTIONS = "BROWSER_OPTIONS";
 
 
     @BeforeClass
     protected void launchBrowser() {
         init_properties();
         final String browserName = properties.getProperty("browser").toLowerCase().trim();
+        final boolean isHeadless = Boolean.parseBoolean(properties.getProperty("headless").toLowerCase().trim());
+        final double isSlow = Double.parseDouble(properties.getProperty("slowMo").toLowerCase().trim());
 
         playwright = Playwright.create();
 
         switch (browserName) {
             case "chromium" -> browser = playwright.chromium().launch(
-                    new BrowserType.LaunchOptions().setHeadless(false));
+                    new BrowserType.LaunchOptions().setHeadless(isHeadless).setSlowMo(isSlow));
             case "firefox" -> browser = playwright.firefox().launch(
-                    new BrowserType.LaunchOptions().setHeadless(false));
+                    new BrowserType.LaunchOptions().setHeadless(isHeadless).setSlowMo(isSlow));
             case "safari" -> browser = playwright.webkit().launch(
-                    new BrowserType.LaunchOptions().setHeadless(false));
+                    new BrowserType.LaunchOptions().setHeadless(isHeadless).setSlowMo(isSlow));
             case "chrome" -> browser = playwright.chromium().launch(
-                    new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(false));
+                    new BrowserType.LaunchOptions().setChannel("chrome").setHeadless(isHeadless).setSlowMo(isSlow));
             default -> System.out.println("Please enter the right browser name...");
         }
     }
@@ -80,16 +87,55 @@ public class BaseTest {
         playwright.close();
     }
 
-    public void init_properties() {
-        try {
-            FileInputStream ip = new FileInputStream("./src/test/resources/config.properties");
+//    public void init_properties() {
+//        try {
+//            FileInputStream ip = new FileInputStream("./src/test/resources/config.properties");
+//            properties = new Properties();
+//            properties.load(ip);
+//        } catch (FileNotFoundException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
+//    }
+
+    private static void init_properties() {
+        if (properties == null) {
             properties = new Properties();
-            properties.load(ip);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            throw new RuntimeException(e);
+            if (isServerRun()) {
+//                properties.setProperty(PROP_CHROME_OPTIONS, System.getenv(ENV_CHROME_OPTIONS));
+
+                if (System.getenv(ENV_BROWSER_OPTIONS) != null) {
+                    for (String option : System.getenv(ENV_BROWSER_OPTIONS).split(";")) {
+                        String[] browserOptionArr = option.split("=");
+                        properties.setProperty(browserOptionArr[0], browserOptionArr[1]);
+                    }
+                }
+
+                if (System.getenv(ENV_WEB_OPTIONS) != null) {
+                    for (String option : System.getenv(ENV_WEB_OPTIONS).split(";")) {
+                        String[] webOptionArr = option.split("=");
+                        properties.setProperty(webOptionArr[0], webOptionArr[1]);
+                    }
+                }
+
+            } else {
+                try {
+                    InputStream inputStream = BaseTest.class.getClassLoader().getResourceAsStream("config.properties");
+                    if (inputStream == null) {
+                        System.out.println("ERROR: The \u001B[31mconfig.properties\u001B[0m file not found.");
+                        System.out.println("You need to create it from config.properties.TEMPLATE file.");
+                        System.exit(1);
+                    }
+                    properties.load(inputStream);
+                } catch (IOException ignore) {
+                }
+            }
         }
+    }
+
+    static boolean isServerRun() {
+        return System.getenv("CI_RUN") != null;
     }
 
     private void login() {
