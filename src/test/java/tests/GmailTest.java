a185fba1e83a753c65eb506867507fcd2c48ba3e
ViktoriyaED <qaviktoriaed@gmail.com>
2024-01-10 12:10:02 -0500

    new account added to GmailUtils111.java

--- src/test/java/tests/GmailTest.java
+++ src/test/java/tests/GmailTest.java
@@ -3,15 +3,17 @@ package tests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 import utils.GmailUtils;
+import utils.GmailUtils111;
 
 public class GmailTest extends BaseTest {
 
     @Test
     public void testExtractGmailPassword() throws Exception {
-        int emailNumber = 1;
-        String expectedPassword = "nvwy602^~(";
-
-        String actualPassword = GmailUtils.extractPasswordFromEmail(GmailUtils.getGmailService(), emailNumber);
-        Assert.assertEquals(actualPassword,expectedPassword);
+//        int emailNumber = 1;
+//        String expectedPassword = "nvwy602^~(";
+//
+//        String actualPassword = GmailUtils.extractPasswordFromEmail(GmailUtils.getGmailService(), emailNumber);
+//        Assert.assertEquals(actualPassword,expectedPassword);
+        GmailUtils.printLabelsInUserAccount(GmailUtils111.getGmailService());
     }
 }
