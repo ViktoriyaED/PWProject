f9cbf5edb8e363bda2742807628b04884e609961
ViktoriyaED <qaviktoriaed@gmail.com>
2024-01-10 23:01:47 -0500

    cleaning GmailUtils

--- src/test/java/tests/GmailTest.java
+++ src/test/java/tests/GmailTest.java
@@ -10,7 +10,7 @@ public class GmailTest {
     @Test
     public void testExtractGmailPassword() throws Exception {
 
-        Assert.assertEquals(GmailReceiver.getPassword(ProjectProperties.USERNAME_GMAIL, ProjectProperties.PASSWORD_GMAIL, "pw.new.test.23@gmail.com"), "PaSsWoRd#");
-//        GmailReceiver.getPassword("pwtester.new@gmail.com", "kbtb ozhw zkjl nkoi", "pw.new.test.23@gmail.com");
+//        Assert.assertEquals(GmailReceiver.getPassword(ProjectProperties.USERNAME_GMAIL, ProjectProperties.PASSWORD_GMAIL, "pw.new.test.23@gmail.com"), "PaSsWoRd#");
+        Assert.assertEquals(GmailReceiver.getPassword("pwtester.new@gmail.com", "kbtb ozhw zkjl nkoi", "pw.new.test.23@gmail.com"), "PaSsWoRd#");
     }
 }
