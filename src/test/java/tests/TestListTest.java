4d9afc8b6f52fdeac321994505383533d20f3132
ViktoriyaED <qaviktoriaed@gmail.com>
2024-01-13 20:09:48 -0500

    adding Allure annotation

--- src/test/java/tests/TestListTest.java
+++ src/test/java/tests/TestListTest.java
@@ -1,15 +1,21 @@
 package tests;
 
+import io.qameta.allure.*;
 import org.testng.annotations.Test;
 import pages.HomePage;
 import pages.TestsPage;
 import utils.ProjectProperties;
 
 import static com.microsoft.playwright.assertions.PlaywrightAssertions.assertThat;
+import static io.qameta.allure.SeverityLevel.CRITICAL;
 
 public class TestListTest extends BaseTest {
 
-    @Test
+    @Test(testName = "TC1344-01 The single non-active Checkbox can be checked.")
+    @Description("Objective: To verify that a non-active checkbox ca be successfully checked.")
+    @Story("Tests")
+    @TmsLink("l3twyfx5esxv")
+    @Owner("ViktoriyaD")
     public void testTutorModeWithRandomCheckboxInDomain() {
         String expectedNumberOfQuestions = "5";
         String testTutorEndPoint = "/test-tutor";
